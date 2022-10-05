117367925b3b45a572966ff7ce8c44c59c6cd5f7
Giacomo Randazzo <giacomoran@gmail.com>
2022-10-05 20:16:15 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  let x = 1; // @hmr:keep
+  x++; // x is now 2
+
+  console.log("x", x);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
